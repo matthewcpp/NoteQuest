a78a7557da4e5246ff0cacc02d42e9a4d0ffc8a7
Matthew LaRocca <mlarocca.cpp@gmail.com>
2020-06-24 12:55:34 -0700

    Add Main Menu

---
+++ Assets/NoteQuest/MainMenu/MainMenuController.cs
@@ -0,0 +1,32 @@
+﻿using NoteQuest;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] ArcadeModeController arcadeModeController;
+    [SerializeField] ScoreModeController scoreModeController;
+    [SerializeField] GameObject mainMenu;
+
+    public void EnterScoreMode()
+    {
+        mainMenu.SetActive(false);
+        scoreModeController.gameObject.SetActive(true);
+    }
+
+    public void EnterArcadeMode()
+    {
+        mainMenu.SetActive(false);
+        arcadeModeController.gameObject.SetActive(true);
+    }
+
+    public void Update()
+    {
+        if (mainMenu.activeSelf)
+            return;
+
+        if (!arcadeModeController.gameObject.activeSelf && !scoreModeController.gameObject.activeSelf)
+            mainMenu.SetActive(true);
+    }
+}
